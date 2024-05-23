861cac0a68a42e2c4b1139780c71f58bfd11d7df
AndresLv04 <166462520+AndresLv04@users.noreply.github.com>
2024-05-23 18:17:21 -0500

    Add files via upload

--- Grafos/src/Main.java
+++ Grafos/src/Main.java
@@ -1,81 +1,51 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-class Dijkstra {
-    int[] distance;
-    boolean[] visited;
-    int[][] cost;
-
-    public Dijkstra(int nodes) {
-        distance = new int[nodes + 1];
-        visited = new boolean[nodes + 1];
-        cost = new int[nodes + 1][nodes + 1];
-    }
-
-    public void calc(int nodes, int source) {
-        for (int i = 1; i <= nodes; i++) {
-            distance[i] = cost[source][i];
-            visited[i] = false;
-        }
-
-        distance[source] = 0;
-        visited[source] = true;
-
-        for (int count = 1; count <= nodes - 1; count++) {
-            int minDistance = 999;
-            int nextNode = -1;
-
-            for (int i = 1; i <= nodes; i++) {
-                if (!visited[i] && distance[i] < minDistance) {
-                    minDistance = distance[i];
-                    nextNode = i;
-                }
-            }
-
-            visited[nextNode] = true;
-
-            for (int i = 1; i <= nodes; i++) {
-                if (!visited[i]) {
-                    int newDistance = distance[nextNode] + cost[nextNode][i];
-                    if (newDistance < distance[i]) {
-                        distance[i] = newDistance;
-                    }
-                }
-            }
-        }
-    }
-}
-
+/**
+ * Clase principal del programa.
+ */
 public class Main {
+
+    /**
+     * Método principal que se ejecuta al iniciar el programa.
+     * @param args Argumentos de línea de comandos.
+     */
     public static void main(String[] args) {
-        int nodes, source, i, j;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the Number of Nodes:");
-        nodes = in.nextInt();
+        // Matriz de adyacencia que representa el grafo. Los valores representan los pesos de las aristas.
+        int[][] matrizAdyacencia = {
+                //   0  1  2  3  4  5
+                /*0*/{0, 6, 4, 0, 0, 0},
+                /*1*/{0, 0, 2, 1, 0, 0},
+                /*2*/{0, 0, 0, 8, 2, 0},
+                /*3*/{0, 0, 0, 0, 0, 4},
+                /*4*/{0, 3, 0, 6, 0, 4},
+                /*5*/{0, 0, 0, 0, 0, 0}
+        };
 
-        Dijkstra d = new Dijkstra(nodes);
+        // Lista para almacenar los nodos del grafo
+        List<Nodo> grafo = new ArrayList<>();
 
-        System.out.println("Enter the Cost Matrix Weights:");
-        for (i = 1; i <= nodes; i++) {
-            for (j = 1; j <= nodes; j++) {
-                d.cost[i][j] = in.nextInt();
-                if (d.cost[i][j] == 0) {
-                    d.cost[i][j] = 999; // 999 represents infinity
+        // Crear los nodos y añadirlos al grafo
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            grafo.add(new Nodo(i));
+        }
+
+        // Crear las aristas basadas en la matriz de adyacencia
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            for (int j = 0; j < matrizAdyacencia[i].length; j++) {
+                int peso = matrizAdyacencia[i][j];
+                if (peso > 0) {
+                    grafo.get(i).vecinos.add(new Arista(grafo.get(j), peso));
                 }
             }
         }
 
-        System.out.println("Enter the Source Vertex:");
-        source = in.nextInt();
+        // Ejecutar el algoritmo de Dijkstra desde el nodo 0
+        Dijkstra.dijkstra(grafo, grafo.get(0));
 
-        d.calc(nodes, source);
-
-        System.out.println("The Shortest Path from Source " + source + " to all other vertices are:");
-        for (i = 1; i <= nodes; i++) {
-            if (i != source) {
-                System.out.println("Source: " + source + " Destination: " + i + " MinCost: " + d.distance[i]);
-            }
+        // Imprimir los resultados
+        for (Nodo nodo : grafo) {
+            System.out.println("Nodo: " + nodo.id + ", Distancia: " + nodo.distancia);
         }
-
-        in.close();
     }
 }
